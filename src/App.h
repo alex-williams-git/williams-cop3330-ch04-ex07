b5e075d826b77e5f0cf5123590e5f568dc22571c
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-11-29 14:19:46 -0500

    added header file

---
+++ src/App.h
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Alexander Williams
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void identifyNumbers();
+
+double getInput();
+
+double doOperation(char operand[], double input1, double input2);
+
+char * getOperation(char operand[]);
+
